caa631e03c76ee3bb9b8de6ac7b482f28f422264
Matt Gross <mbgross111@gmail.com>
2021-08-31 20:00:59 -0700

    get time series daily for one ticker and generate csv

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 require('dotenv').config();
+const request = require('request');
+const { parseAsync } = require('json2csv');
+const writeCsv = require('./utils/write-csv');
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 
-console.log(process.env.API_KEY)
+const alphaFunction = 'TIME_SERIES_DAILY';
+const alphaSymbol = 'IBM';
+const url = `https://www.alphavantage.co/query?function=${alphaFunction}&symbol=${alphaSymbol}&apikey=${process.env.API_KEY}`;
+
+request.get({
+  url: url,
+  json: true,
+  headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    // data is successfully parsed as a JSON object:
+    const object = data["Time Series (Daily)"];
+
+    const csvData = [];
+
+    for (const key in object) {
+      csvData.push(
+        { symbol: alphaSymbol, 'date': key, ...object[key] }
+      );
+    }
+
+    console.log(csvData);
+
+
+    parseAsync(csvData)
+      .then(csv => {
+        writeCsv(csv)
+          .then(res => console.log(res.message))
+          .catch(err => console.log(err));
+      });
+  }
+});
+
